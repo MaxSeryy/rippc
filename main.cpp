b69636d65fdab43c5dd5d6a0d116bc8f31c0050a
MaxSeryy <maxsiryk@stu.cn.ua>
2024-11-26 19:23:10 +0200

    Init

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <conio.h>
+#include <iomanip>
+#include <windows.h>
+using namespace std;
+
+int menu(){
+    cout <<"\n^-^ R.I.P. YOUR PC ^-^" << endl;
+    cout <<"1. Test your PC" << endl;
+    cout <<"2. How mach free PHYSICAL memory" << endl;
+    cout <<"3. Exit" << endl;
+}
+
+int freeMemory(){
+    MEMORYSTATUSEX statex;
+    statex.dwLength = sizeof(statex);
+    GlobalMemoryStatusEx(&statex);
+    cout << "There is " << statex.ullAvailPhys / 1024 / 1024 << " MB of PHYSICAL memory available." << endl;
+    return 0;
+}
+
+int test(){
+    int *arr;
+    int p = 3.141592653589793238462643;
+    double pi = 3.141592653589793238462643;
+
+    while(true){
+        arr = new int[1000000];
+        for(int i = 0; i < 1000000; i++){
+            arr[i] = i;
+            if(arr[i] % p * pi == 0){
+                arr[i] = arr[i] / p;
+            }
+        }
+    }
+    return 0;
+}
+
+int main(){
+    int choice;
+    do{
+        menu();
+        cout << "Enter your choice: ";
+        cin >> choice;
+        switch(choice){
+            case 1:
+                cout << "Test" << endl;
+                test();
+                break;
+            case 2:
+                cout << "Free Memory" << endl;
+                freeMemory();
+                break;
+            case 3:
+                cout << "Exit" << endl;
+                break;
+            default:
+                cout << "Invalid choice" << endl;
+        }
+    }while(choice != 3);
+    return 0;
+}
